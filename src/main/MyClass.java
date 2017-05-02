ed5094fc361803285e33b14960a0b1294b08408d
javregvar <jvireg@gmail.com>
2017-05-02 19:27:35 +0200

    Update MyClass.java

--- src/main/MyClass.java
+++ src/main/MyClass.java
@@ -1,14 +1,8 @@
 public class Pruebas {
 	
 	public static void cuentaHastaN(int N){
-		if(N>0){
-			//Si es positivo
-			for(int i=1;i<=N;i++){
-				System.out.println(i);
-			}
-		}else{
-			//Si es negativo
-			System.out.println("El número introducido es negativo o cero.");
+		for(int i=1;i<=N;i++){
+			System.out.println(i);
 		}
 	}
 	
