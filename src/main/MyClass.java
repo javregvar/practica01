464c7b4f6dbc66a54d7c88ed203b3b47d2b05cf1
javregvar <jvireg@gmail.com>
2017-05-02 19:29:59 +0200

    Update MyClass.java

--- src/main/MyClass.java
+++ src/main/MyClass.java
@@ -1,14 +1,21 @@
 public class Pruebas {
-	
-	public static void cuentaHastaN(int N){
+
+	public static void cuentaHastaN(int N) throws Exception{
 		for(int i=1;i<=N;i++){
 			System.out.println(i);
 		}
 	}
 	
 	public static void main(String[] args){
-		cuentaHastaN(5);
-		cuentaHastaN(-5);
+		try{
+			int n = 5;
+			cuentaHastaN(n);
+			cuentaHasta10();
+			
+			n=-5;
+			cuentaHastaN(n);
+		}catch(Exception e){
+			
+		}
 	}
-	
 }
